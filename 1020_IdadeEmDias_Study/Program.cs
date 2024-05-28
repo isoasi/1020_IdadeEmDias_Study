abcb4e99d7290859410aacd96ece3ed948224e7d
paola <paola@JORDANIAVMROCHA>
2024-05-27 22:31:41 -0300

    Adicionar arquivos de projeto.

---
+++ 1020_IdadeEmDias_Study/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI{ 
+
+    static void Main(string[] args) {
+
+        int N, anos, meses, dias, resto;
+
+        N = int.Parse(Console.ReadLine());
+
+        anos = N / 365;
+        resto = N % 365;
+
+        meses = resto / 30;
+        dias = resto % 30;
+
+        Console.WriteLine(anos + " ano(s)");
+        Console.WriteLine(meses + " mes(es)");
+        Console.WriteLine(dias + " dia(s)");
+
+        Console.WriteLine();
+
+    }
+    
+}
